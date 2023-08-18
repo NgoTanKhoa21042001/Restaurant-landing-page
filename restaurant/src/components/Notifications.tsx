ed4f5a606d43ff99732e12f9a7ec58ba8717496e
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-08-18 17:45:31 +0700

    Responsive Navbar Menu

---
+++ restaurant/src/components/Notifications.tsx
@@ -0,0 +1,9 @@
+const Notifications = () => {
+  return (
+    <div className="h-12 bg-red-500 text-white px-4 flex items-center justify-center">
+      Free delivery for all orders over $50. Order your food now!
+    </div>
+  );
+};
+
+export default Notifications;
