3872a37055b19fb4ef111ebe4dc7818416db037f
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-08-19 21:01:26 +0700

    Featured Products Design

--- restaurant/src/components/Slider.tsx
+++ restaurant/src/components/Slider.tsx
@@ -33,10 +33,10 @@ const Slider = () => {
     <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]">
       {/* Text */}
       <div className="h-1/2 flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
-        <h1 className="text-5xl text-center uppercase p-4">
+        <h1 className="md:text-5xl text-2xl text-center uppercase p-4">
           {data[currentSlide].title}
         </h1>
-        <button className="bg-red-500 text-white p-4 text-5xl md:text-6xl xl:text-7xl">
+        <button className="bg-red-500 text-white p-4 text-3xl md:text-6xl xl:text-7xl">
           Order Now
         </button>
       </div>
